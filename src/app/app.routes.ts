b77814df862f45dc35675133c09b8c2147db2f3b
CODELABSYS\Ahmed.ElBakrey <ahmed.elbakrey@codelabsys.com>
2025-08-24 13:45:59 +0300

    #34 Master Angular: Wildcard Routes, Titles, and Configuring (Part 02)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,15 @@ import { AboutComponent } from './about/about.component';
 import { SwimmingComponent } from './swimming/swimming.component';
 import { GizaComponent } from './giza/giza.component';
 import { AlexComponent } from './alex/alex.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: 'alex', component:AlexComponent},
-    {path:'about', component:GizaComponent}
+
+    {path:'',  redirectTo:'home', pathMatch:'full'},
+    {path:'home', component:HomeComponent, title:'Home Page'},
+    {path: 'alex', component:AlexComponent, title:'Alex Page'},
+    {path:'giza', component:GizaComponent, title:'Giza Page'},
+
+    {path:'**', component:SwimmingComponent, title:'Swimming Page'},
+
 ];
